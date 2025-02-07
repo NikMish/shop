0fc2051eebe5579849a3bac67a2dfc77109f3321
nikmish <misharev@hotmail.com>
2025-02-06 16:24:47 -0800

    Changes to package.

--- react/index.js
+++ react/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from "react-dom/client";
 import ShopApp from './shop';
