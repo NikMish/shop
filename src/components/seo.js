7286e913f5aea25a7ff16a7e8d9b581a959f7e36
NikMish <misharev@hotmail.com>
2026-08-26 16:16:50 -0700

    Added cononical link tag - plus.

--- src/components/seo.js
+++ src/components/seo.js
@@ -95,7 +95,7 @@ const Seo = ({ description, title, ogimage, slug, children, product }) => {
   
   return (
     <>
-      <link rel="canonical" href="https://misharev.com" />
+      <link rel="canonical" href={`https://misharev.com${slug ? `/${slug}` : ''}`} />
       <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
       <meta property="og:title" content={title} />
