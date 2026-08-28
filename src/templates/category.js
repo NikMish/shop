6eef4b0294835d1e28782b4d455a69bf0098b57c
NikMish <misharev@hotmail.com>
2026-08-28 15:17:11 -0700

    Added fallback alt text.

--- src/templates/category.js
+++ src/templates/category.js
@@ -48,9 +48,9 @@ const ShopIndex = ({ data, pageContext, path }) => {
                       const file = fileMap.get(imgUrl)
                       if (file && file.childImageSharp) {
                         const img = getImage(file.childImageSharp.gatsbyImageData)
-                        return <GatsbyImage image={img} alt={title} />
+                        return <GatsbyImage image={img} alt={title || 'Unique and handmade item'} />
                       }
-                      return <img src={`/${imgUrl}`} alt={title} />
+                      return <img src={`/${imgUrl}`} alt={title || 'Unique and handmade item'} />
                     })()}
 
                     {node.sold && (
